68c81918598c91520548d801cdce27d409a62b6d
SamDz16 <samdz161999@gmail.com>
2020-07-09 19:27:49 +0100

    adding button for submitting a comment about the author inside a modal

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,111 @@
-import React from 'react';
-import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, { Component } from 'react';
+import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Button, Label, Col, Row, Modal, ModalHeader, ModalBody } from 'reactstrap';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 
+
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => (val) && (val.length >= len);
+
+class CommentForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false
+    }
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(values) {
+    console.log("Current State is : " + JSON.stringify(values));
+    alert("Current State is : " + JSON.stringify(values));
+  };
+
+  toggleModal() {
+    this.setState({ isModalOpen: !this.state.isModalOpen });
+  };
+
+  render() {
+    return (
+      <>
+        <Button outline onClick={this.toggleModal}>
+          <span className="fa fa-comment fa-lg"></span> Submit Comment
+        </Button>
+
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+
+          <ModalBody>
+
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+
+              <Row className="form-group">
+
+                <Label md={12} htmlFor="author">Raiting</Label>
+                <Col>
+                  <Control.select model=".raiting" name="raiting"
+                    className="form-control">
+
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+
+                  </Control.select>
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Label htmlFor="author" md={12}>Your Name</Label>
+                <Col>
+                  <Control.text model=".author" id="author" name="author"
+                    placeholder="Your Name"
+                    className="form-control"
+                    validators={{
+                      minLength: minLength(3), maxLength: maxLength(15)
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".author"
+                    show="touched"
+                    messages={{
+                      minLength: 'Must be greater than 2 characters',
+                      maxLength: 'Must be 15 characters or less'
+                    }} />
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Label htmlFor="message" md={12}>Your Feedback</Label>
+                <Col>
+                  <Control.textarea model=".comment" id="comment" name="comment"
+                    rows="6"
+                    className="form-control" />
+                </Col>
+              </Row>
+
+              <Row className="form-group">
+                <Col>
+                  <Button type="submit" color="primary">
+                    Submit
+                  </Button>
+                </Col>
+              </Row>
+
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </>
+    );
+  }
+};
 
 function RenderDish({ dish }) {
   return (
@@ -31,6 +135,7 @@ function RenderComments({ comments }) {
     <div className="col-12 col-md-5 m-1">
       <h4>Comments</h4>
       {cmnts}
+      <CommentForm />
     </div>
   );
 };
