437360cb86244830664b5689941498b7c369ed07
SamDz16 <samdz161999@gmail.com>
2020-07-10 20:19:03 +0100

    Reducers

---
+++ src/redux/dishes.js
@@ -0,0 +1,10 @@
+import { LEADERS } from '../shared/Leaders';
+import { DISHES } from '../shared/dishes';
+
+
+export const Dishes = (state = DISHES, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
---
+++ src/redux/leaders.js
@@ -0,0 +1,9 @@
+import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/Leaders';
+
+export const Leaders = (state = LEADERS, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
